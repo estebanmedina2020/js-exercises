cb0c1984bf3778bc6b3734469704ebdb5be2fcbc
Esteban <emedina@creatonico.com>
2020-04-03 12:00:56 +0200

    resolve exercide D-variables with a different approach

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -2,4 +2,10 @@
 //var greetingOne = "Hello world";
 //var greetingTwo = "Hello world";
 //var greetingThree = "Hello world";
-console.log("Hello world\nHello world\nHello world");
+//console.log("Hello world\nHello world\nHello world");
+
+let greeting = "Hello world";
+
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
