70690cd91464e6b3cdea3f96effb9e578db0b83f
Esteban <emedina@creatonico.com>
2020-04-03 22:43:49 +0200

    finish I-floats exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,33 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+//let numberOfStudents = 15;
+//let numberOfMentors = 8;
+
+// let numberOfStudents = 100;
+// let percentToGet = 65;
+// let percent = (percentToGet / 100) * numberOfStudents;
+// console.log("Percentage students:", percentToGet + "%");
+
+//console.log(numberOfStudents + numberOfMentors);
+
+let numberOfStudents = 15;
+let numberOfMentors = 8;
+let numberOfPeople = 23;
+
+// console.log(numberOfStudents / numberOfPeople);
+
+let students = 0.6521739130434783;
+let roundedNumberOfStudents = Math.round(students);
+
+// console.log((numberOfStudents / numberOfPeople) * 100);
+
+let studentPercentage = 65.21739130434783;
+let roundedStudentPercentage = Math.round(studentPercentage);
+
+console.log("Percentage students:", roundedStudentPercentage + "%");
+
+// console.log(numberOfMentors / numberOfPeople);
+
+let mentorsPercentage = 34.782608695652173;
+let roundedMentorsPercentage = Math.round(mentorsPercentage);
+// console.log(roundedMentorsPercentage);
+
+console.log("Percentage mentors:", roundedMentorsPercentage + "%");
