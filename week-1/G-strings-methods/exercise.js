beccb2afc9befee0c92c8568edc0fa9c94cdbec1
Esteban <emedina@creatonico.com>
2020-04-03 15:03:31 +0200

    finish the first exercise of G-strings-methods

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,17 @@
 // Start by creating a variable `message`
+//var name = "Daniel";
+//var nameLength = name.length;
 
-console.log(message);
+//console.log(nameLength);
+
+// var name = "DANIEL";
+// var nameLowerCase = name.toLowerCase();
+
+// console.log(nameLowerCase);
+
+let message01 = "My name is Esteban and my name is";
+let name = "Esteban";
+let nameLength = name.length;
+let message02 = "characters long";
+
+console.log(message01, nameLength, message02);
