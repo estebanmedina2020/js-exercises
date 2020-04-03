2483df4f0297a68cc7118cc4708ece580ef96199
Esteban <emedina@creatonico.com>
2020-04-03 12:26:13 +0200

    finish exercise F-strings-concatenation

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+let greetingStart = "Hello, my name is ";
+let name = "Esteban";
 
-console.log(message);
+var greeting = greetingStart + name;
+
+console.log(greeting);
