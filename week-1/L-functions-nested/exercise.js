9ff9e66dd0aa65695182377a6d93e2a09e072784
Esteban <emedina@creatonico.com>
2020-04-04 18:54:10 +0200

    finish first L-functions-nested exercise

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,34 @@
+/*
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+*/
+
+let students = 15;
+let mentors = 8;
+let people = 23;
+function studentPercentage(students, people) {
+  return (students / people) * 100;
+}
+function roundedStudentPercentage(percentage) {
+  return Math.round(percentage);
+}
+roundedStudentPercentage(studentPercentage(students, people));
+console.log(
+  "Percentage students: " +
+    roundedStudentPercentage(studentPercentage(students, people))
+);
+
+function mentorsPercentage(mentors, people) {
+  return (mentors / people) * 100;
+}
+function roundedMentorsPercentage(percentage) {
+  return Math.round(percentage);
+}
+roundedMentorsPercentage(mentorsPercentage(mentors, people));
+console.log(
+  "Percentage mentors: " +
+    roundedMentorsPercentage(mentorsPercentage(mentors, people))
+);
