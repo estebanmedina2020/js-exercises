7f0332819824b43b604ebfc0590bea641e1a2066
Esteban <emedina@creatonico.com>
2020-04-04 16:49:36 +0200

    finish fifth K-functions-parameters exercise

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,5 +1,13 @@
 // Declare your function here
 
-const greeting = createLongGreeting("Daniel", 30);
+// const greeting = createLongGreeting("Daniel", 30);
 
-console.log(greeting);
+// console.log(greeting);
+
+const name = "Daniel";
+const age = 30;
+function longGreeting(name, age) {
+  return "Hello, my name is " + name + " and I am " + age + " years old";
+}
+longGreeting(name, age);
+console.log(longGreeting(name, age));
