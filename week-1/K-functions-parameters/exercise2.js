481ee2670a99623ea65780c5cfba71ea5e5e16c6
Esteban <emedina@creatonico.com>
2020-04-04 13:19:27 +0200

    finish second K-functions-parameters exercise

--- week-1/K-functions-parameters/exercise2.js
+++ week-1/K-functions-parameters/exercise2.js
@@ -1,5 +1,12 @@
 // Declare your function first
 
-var result = divide(3, 4);
+// var result = divide(3, 4);
 
-console.log(result);
+// console.log(result);
+
+function divideTwoNumbers(a, b) {
+  let result = a / b;
+  console.log(result);
+}
+
+divideTwoNumbers(3, 4);
