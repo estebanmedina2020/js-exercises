5c9357f4c94d29e7f931df32cdbd2abcd40da440
Esteban <emedina@creatonico.com>
2020-04-04 13:46:55 +0200

    finish fourth K-funtions-parameters exercise

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -2,4 +2,11 @@
 
 // Call the function and assign to a variable `sum`
 
-console.log(sum);
+// console.log(sum);
+
+function sumTwoNumbers(a, b) {
+  let result = a + b;
+  console.log(result);
+}
+
+sumTwoNumbers(13, 124);
