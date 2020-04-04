74e25c363500b06a2db8da7f10f3a43783d6ae6d
Esteban <emedina@creatonico.com>
2020-04-04 12:16:03 +0200

    finish 1st exercise of K-functions-parameters

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,9 +1,14 @@
 // Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
-}
+//function multiply() {//return a * b;}
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+//var result = multiply(3, 4);
 
-console.log(result);
+//console.log(result);
+
+function multiplyTwoNumbers(a, b) {
+  let result = a * b;
+  console.log(result);
+}
+
+multiplyTwoNumbers(2, 6);
