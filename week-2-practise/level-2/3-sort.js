3de1486201c0ad7af4316e9ea68159ee0ac5bb65
Esteban <emedina@creatonico.com>
2020-04-14 11:51:28 +0200

    finish level-2 sort exercise

--- week-2-practise/level-2/3-sort.js
+++ week-2-practise/level-2/3-sort.js
@@ -3,7 +3,7 @@
 
 let letters = ["a", "n", "c", "e", "z", "f"];
 let letters2 = ["w", "b", "v", "g", "l", "o"];
-let sortedLetters = sortedLetters.sort(letters + letters2); // ONLY EDIT THIS LINE
+let sortedLetters = letters.concat(letters2).sort(); // ONLY EDIT THIS LINE
 
 console.log(sortedLetters);
 
@@ -12,11 +12,3 @@ console.log(sortedLetters);
   ---------------
   [ 'a', 'b', 'c', 'e', 'f', 'g', 'l','n', 'o', 'v', 'w', 'z' ]
 */
-
-var letters = ["a", "n", "c", "e", "z", "f"];
-var letters2 = ["w", "b", "v", "g", "l", "o"];
-var sortedLetters = sort(function (letters, letters2) {
-  return letters[""] + letters2[""];
-}); // ONLY EDIT THIS LINE
-
-console.log(sortedLetters);
