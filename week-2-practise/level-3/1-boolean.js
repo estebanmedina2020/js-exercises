8989fd73d24b02d2a906bb773fd37a84ee5c3650
Esteban <emedina@creatonico.com>
2020-04-15 16:18:00 +0200

    finish level-3 boolean exercises

--- week-2-practise/level-3/1-boolean.js
+++ week-2-practise/level-3/1-boolean.js
@@ -3,9 +3,13 @@
 // What was wrong with the original code?
 // Leave comments above your changes to explain
 
-var isHappy = "false";
+// To make it a more accurate code we could set the statement to "true"
+// Then make the conditionals run using the "if" and "else"
+// Making the first condition "false" in order for else to be executed
 
-if (isHappy == true) {
+let isHappy = "true";
+
+if (isHappy == false) {
   console.log("I am happy");
 } else {
   console.log("I am not happy");
