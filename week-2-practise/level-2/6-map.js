8ead905a812189fba6dee3bc2a900b239bb9088b
Esteban <emedina@creatonico.com>
2020-04-15 14:29:26 +0200

    finish level-2 map exercise

--- week-2-practise/level-2/6-map.js
+++ week-2-practise/level-2/6-map.js
@@ -3,12 +3,15 @@
 // 2. Declare a new array, `percentagesFormatted`, containing
 //    - each item in `percentages` formatted by your function
 
-var percentages = [1, 23, 92, 18];
+let percentages = [1, 23, 92, 18];
+let percentagesFormatted = percentages.map(function (number) {
+  return number + "%";
+});
 
 console.log(percentagesFormatted);
 
 /* 
   EXPECTED RESULT
   ---------------
-  [1%, 23%, 92%, 18%]
+  ['1%', '23%', '92%', '18%']
 */
